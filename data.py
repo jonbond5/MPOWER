d84848497afdfe021b6b7fbb8669ca892dc71344
jonbond5 <jgonsalves575@gmail.com>
2017-01-26 10:23:08 -0500

    Include data parsing from data.py

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ class Data:
     
     def __init__(self, file_string):
         self.file_string = file_string
-        self.data = pd.read_csv(SIO(self.file_string))
+        self.data = pd.read_csv(self.file_string)
         self.columns = list(self.data.columns.values)
 
     def getData(self):
@@ -17,3 +17,33 @@ class Data:
     def getColumns(self):
         return self.columns
 
+    def getSubdata(self, column):
+        return self.data[column]
+
+def test():
+    print """
+    Methods:
+    getData - returns DataFrame 
+    getColums - returns column names of the DataFrame
+    getSubdata(column) - requires column, returns data of a certain column
+
+    Files:
+    Filenames are from 2006-2016
+
+    To quit:
+    Enter quit
+    """
+
+    while 1:
+        option = raw_input('What would you like to view: ')
+        filename = raw_input('What file are we dealing with: ')
+        d = Data(filename)
+        if option == 'getData':
+            d.getData()
+        elif option == 'getColumns':
+            d.getColumns()
+        elif option == 'getSubdata':
+            column = raw_input('Column name: ')
+            d.getSubdata(column)
+        elif option == 'quit':
+            break
---
+++ run.bat
@@ -0,0 +1,1 @@
+python -c "import data; data.test()"
