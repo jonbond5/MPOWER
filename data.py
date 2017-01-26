e3f3b676d7a3bea738ee3b795bea11098f60da84
jonbond5 <jgonsalves575@gmail.com>
2017-01-26 09:04:29 -0500

    Add data.py

---
+++ data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from StringIO import StringIO as SIO
+
+class Data:
+    """
+    Object that parses the data of a given file, organizes each column into a pandas DF
+    """
+    
+    def __init__(self, file_string):
+        self.file_string = file_string
+        self.data = pd.read_csv(SIO(self.file_string))
+        self.columns = self.data.keys()
+
+    def getData(self):
+        return self.data
+
+    def getColumns(self):
+        return self.columns
+        
+    def 
